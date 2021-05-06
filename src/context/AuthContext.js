5e9a5d19c270a1450a7c722e4c13ffb2dc533175
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-05-06 14:01:48 +0200

    Create stappenplan

---
+++ src/context/AuthContext.js
@@ -0,0 +1,9 @@
+// STAPPENPLAN CONTEXT KLAARZETTEN (2)
+// 0. [x] AuthContext.js bestand aanmaken
+// 1. [ ] AuthContext maken met createContext
+// 2. [ ] AuthContextProvider functie component bouwen met daarin:
+//    - [ ] Het echte AuthContext.Provider component
+//    - [ ] Een leeg data object
+// 3. [ ] Geef het data object mee via de value={} property in de .Provider
+// 4. [ ] Exporteer zowel de Context als het zelfgemaakte Provider component
+// 5. [ ] Importeer het zelfgemaakte Provider component in index.js en wikkel deze om <App> heen
