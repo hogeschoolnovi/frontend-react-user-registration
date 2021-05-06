a4b60276b9b6908cb3c04fdaeb9ddd1f550d315d
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-05-06 17:09:14 +0200

    Implement stappenplan

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -8,6 +8,20 @@ import { AuthContext } from '../context/AuthContext';
 // - [x] Bekijk hoe de data in de context eruit ziet doormiddel van alles = useContext(AuthContext) en console.log dit!
 // - [x] Welke onderdelen zie je in de console?
 
+// STAPPENPLAN INLOGGEN (5)
+// - [ ] Importeer axios
+// - [ ] Maak een asynchrone functie
+// - [ ] Maak een try / catch blok
+// - [ ] In de try: maak een POST request naar het eindpoint: http://localhost:3000/login
+// - [ ] Een POST request krijgt altijd de url en het data object mee (in dit geval alleen email en wachtwoord)
+// - [ ] Bekijk de response. Als het succesvol was, dan:
+// - [ ] Moet de JWT worden doorgegeven aan de context vanuit hier:
+//    - [ ] Importeeer useContext en AuthContext
+//    - [ ] Destructure daar de login functie uit
+//    - [ ] Roep deze functie aan als het inloggen succesvol was en geef de JWT token daaraan mee
+// - Wanneer alles in de context goed gaat, zullen we ook vanuit daar de gebruiker doorlinken naar de profielpagina.
+// - Puntjes op de i: error en laad-tijden inplemententeren (maar dit kun je inmiddels zelf!)
+
 function SignIn() {
   const { handleSubmit, register } = useForm();
   const alles = useContext(AuthContext);
