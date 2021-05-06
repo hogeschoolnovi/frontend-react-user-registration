431fc29d54c12d84289cd7e78c66b7154921e37a
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-05-06 14:19:23 +0200

    Create authState

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,16 +1,21 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const AuthContext = createContext({});
 
 // STAPPENPLAN CONTEXT INRICHTEN (3)
 // - [x] Bedenk welke data je in de context beschikbaar moet stellen
 // - [x] Maak de lege functies voor login en logOut
-// - [ ] Maak de state aan voor de gebruikersdata en de statusdata (user => null en status => 'pending')
+// - [x] Maak de state aan voor de gebruikersdata en de statusdata (user => null en status => 'pending')
 // - [ ] Maak ook alvast een useEffect functie die de status op 'done' zet als de app gerefreshed wordt (mounting cycle)
 // - [ ] Zorg ervoor dat we alleen de applicatie (dus de children) laten zien als de status op 'done' staat
 // - [ ] Plaats de state en lege functies in het data object
 
 function AuthContextProvider({ children }) {
+  const [ authState, setAuthState ] = useState({
+    user: null,
+    status: 'pending',
+  })
+
   function login() {
     console.log('login!');
   }
