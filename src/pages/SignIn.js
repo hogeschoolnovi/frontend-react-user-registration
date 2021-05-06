b8995f991385be117488ab9a86b2a3e7f8d7402e
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-05-06 17:13:37 +0200

    Instead of alles, only get the login function from the context

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -16,17 +16,16 @@ import { AuthContext } from '../context/AuthContext';
 // - [x] In de try: maak een POST request naar het eindpoint: http://localhost:3000/login
 // - [x] Een POST request krijgt altijd de url en het data object mee (in dit geval alleen email en wachtwoord)
 // - [x] Bekijk de response. Als het succesvol was, dan:
-// - [ ] Moet de JWT worden doorgegeven aan de context vanuit hier:
-//    - [ ] Importeeer useContext en AuthContext
-//    - [ ] Destructure daar de login functie uit
+// - [x] Moet de JWT worden doorgegeven aan de context vanuit hier:
+//    - [x] Importeeer useContext en AuthContext
+//    - [x] Destructure daar de login functie uit
 //    - [ ] Roep deze functie aan als het inloggen succesvol was en geef de JWT token daaraan mee
 // - Wanneer alles in de context goed gaat, zullen we ook vanuit daar de gebruiker doorlinken naar de profielpagina.
 // - Puntjes op de i: error en laad-tijden inplemententeren (maar dit kun je inmiddels zelf!)
 
 function SignIn() {
   const { handleSubmit, register } = useForm();
-  const alles = useContext(AuthContext);
-  console.log(alles); // geeft een object met vier keys: login, logout, status en user
+  const { login } = useContext(AuthContext);
 
   async function onSubmit(data) {
     console.log(data);
