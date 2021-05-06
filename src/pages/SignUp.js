615b609fe63b27de2b08dfc450a0361d2b405536
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-05-06 13:03:50 +0200

    Install and import axios

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 // STAPPENPLAN REGISTREREN (1)
-// - [ ] Installeer axios
-// - [ ] Importeer axios
+// - [x] Installeer axios
+// - [x] Importeer axios
 // - [ ] Maak een asynchrone functie
 // - [ ] Maak een try / catch blok
 // - [ ] In de try: maak een POST request naar het eindpoint: http://localhost:3000/register
